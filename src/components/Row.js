5b24c9f52030349689a46f5669a6b9939888f4c1
Karandeep Lubana <karandeepLubana1@gmail.com>
2021-08-12 23:40:25 -0400

    changes

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,6 +1,20 @@
 import React, { useEffect, useState } from "react";
 import axios from "../axios";
 import ResultCard from "./ResultCard";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/swiper.min.css";
+import "swiper/components/pagination/pagination.min.css";
+
+import "../App.css";
+
+// import Swiper core and required modules
+import SwiperCore, { Pagination } from "swiper/core";
+
+// install Swiper modules
+SwiperCore.use([Pagination]);
 
 const Row = ({ title, fetchUrl }) => {
   const [results, setResults] = useState([]);
@@ -9,7 +23,8 @@ const Row = ({ title, fetchUrl }) => {
     // Fetch data from fetchurl
     const fetchData = async () => {
       const response = await axios(fetchUrl);
-      console.log(response.data.results);
+      // console.log(response.data.results);
+      response.data.results.sort(() => Math.random() - 0.5);
       setResults(response.data.results);
     };
     fetchData();
@@ -20,13 +35,23 @@ const Row = ({ title, fetchUrl }) => {
       <h1 className="row-title">{title}</h1>
 
       {results.length > 0 && (
-        <ul className="results">
+        <Swiper
+          slidesPerView={5.3}
+          spaceBetween={0}
+          freeMode={true}
+          pagination={{
+            clickable: true,
+          }}
+          className="mySwiper"
+        >
           {results.map((movie) => (
-            <li key={movie.id}>
-              <ResultCard movie={movie} />
-            </li>
+            <SwiperSlide key={movie.id}>
+              <li key={movie.id}>
+                <ResultCard movie={movie} />
+              </li>
+            </SwiperSlide>
           ))}
-        </ul>
+        </Swiper>
       )}
     </div>
   );
